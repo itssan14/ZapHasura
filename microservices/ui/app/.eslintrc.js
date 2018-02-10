b88de31cf9047ca42d24dc0803e8df158547280b
Sanjay S <sanjaylachat@gmail.com>
2018-02-10 19:52:26 +0530

    Adding Mobile Authentication

--- microservices/ui/app/.eslintrc.js
+++ microservices/ui/app/.eslintrc.js
@@ -1,14 +1,13 @@
 module.exports = {
-  extends: ["airbnb", "prettier"],
-  plugins: "prettier",
-  rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        singleQuote: true,
-        trailingComma: "all"
-      }
-    ],
-    eqeqeq: ["error", "always"]
-  }
-};
+	extends: ['airbnb', 'prettier'],
+	plugins: 'prettier',
+	rules: {
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true
+			}
+		],
+		eqeqeq: ['error', 'always']
+	}
+}
